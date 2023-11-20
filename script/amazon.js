0d8cd15ffff28561474505239e0744badd64ad18
rian911 <riansofc@gmail.com>
2023-11-20 15:31:12 +0700

    get link to data

--- script/amazon.js
+++ script/amazon.js
@@ -6,45 +6,6 @@ main idea of javascript
 3. make it interactive
 */
 
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      stars: 4.0,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-  {
-    image: 'images/products/black-2-slot-toaster.jpg',
-    name: '2 Slot Toaster - Black',
-    rating: {
-      stars: 5.0,
-      count: 2197,
-    },
-    priceCents: 1899,
-  },
-];
-
 /* gunanya foreach itu mengambil setiap object 
 didalam variabel products dan menyimpannya 
 di dalam parameter product kemudian menjalankan sebuah fungsi */
