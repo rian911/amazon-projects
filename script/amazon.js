e0d2e212b5fa9443dd4b1afff1db30de718b5d6f
rian911 <riansofc@gmail.com>
2023-11-28 19:03:16 +0700

    create module function

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { addtoCart, updateCartQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
 /* 
 this node
@@ -66,62 +66,6 @@ products.forEach((product) => {
     }">Add to Cart</button>
   </div>`;
 });
-
-function addtoCart(productId) {
-  // Inisialisasi variabel untuk menyimpan item yang sama dalam keranjang
-  let matchingItem;
-
-  // dibawah ini code untuk mengecek apakah ada product yang sama ketika ditambahkan kedalam cart array (apakah didalam cart array sudah ada product yg sama?)
-  // Iterasi melalui setiap item dalam keranjang
-  // parameter item ini berisi productName dan quantity
-  cart.forEach((item) => {
-    // Memeriksa apakah nama produk saat ini sama dengan produk yang akan ditambahkan
-    if (productId === item.productId) {
-      // Jika sama, simpan referensi ke item tersebut
-      matchingItem = item;
-    }
-  });
-
-  // 1. tadi salah lu naroh diatas jadinya productid gk kedetec undefined
-  // 2. productid penulisannya juga salah, awalnya nulis product.id kan harusnya productID berubah wey
-
-  // ini untuk memasukan nilai dari selected value ke variabel quantitySelector
-  const quantitySelecor = document.querySelector(
-    `.js-quantity-selector-${productId}`
-  );
-
-  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-
-  addedMessage.classList.add('added-to-cart-visible');
-  setTimeout(() => {
-    addedMessage.classList.remove('added-to-cart-visible');
-  }, 2000);
-  // nah kan hasil value tadi bentuknya string harus berubah jadi number pake cara ini dan disimpan ke variabel selectedQuantity
-  const selectedQuantity = Number(quantitySelecor.value);
-
-  // Setelah iterasi selesai, periksa apakah ada item yang cocok
-  if (matchingItem) {
-    // Jika ada, tambahkan satu unit ke quantity item yang cocok
-    matchingItem.quantity += selectedQuantity;
-  } else {
-    // Jika tidak ada item yang cocok, tambahkan produk baru ke dalam keranjang
-    cart.push({
-      // namanya bebas, valuenya dari const diatas
-      //destructuring code ini dibawah ini
-      productId: productId,
-      quantity: selectedQuantity,
-    });
-  }
-}
-
-function updateCartQuantity() {
-  let cartQuantity = 0;
-  cart.forEach((item) => {
-    cartQuantity += item.quantity;
-  });
-  document.querySelector('.cart-quantity').innerHTML = cartQuantity;
-  console.log(cart);
-}
 
 // merubah isi didalam class products-grid menjadi productsHTML
 document.querySelector('.products-grid').innerHTML = productsHTML;
