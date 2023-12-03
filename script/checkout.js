b618745279d7b912d364b4e0598d2727b54df62f
rian911 <riansofc@gmail.com>
2023-12-03 19:51:24 +0700

    default export example

--- script/checkout.js
+++ script/checkout.js
@@ -7,6 +7,7 @@ import {
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 hello();
 const day = dayjs();
--- script/utils/money.js
+++ script/utils/money.js
@@ -1,3 +1,5 @@
 export function formatCurrency(PriceCents) {
   return (PriceCents / 100).toFixed(2);
 }
+
+// export default formatCurrency; contoh default export
